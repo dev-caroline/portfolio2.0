886551aaa531b433fc669b336d397d28814d9758
Caroline <ajiboyecaroline95@gmail.com>
2026-04-26 02:19:23 +0100

    working

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='border border-gray-900 p-5 w-405 flex justify-between'>
+        <div className='font-serif text-xl'>`//* Welcome to the world of systems 💡</div>
+        <div>icons</div>
+    </div>
+  )
+}
+
+export default Navbar
